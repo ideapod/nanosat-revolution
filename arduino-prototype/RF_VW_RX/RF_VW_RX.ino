38724c92606f3cd478d7874107002bd460e769d9
Mark Smith <ideapod@optusnet.com.au>
2017-01-14 09:25:04 +1100

    Working sketches, details products used
    I updated the comment headers to remove the original headers from Mike
    McAuley, but I retained acknowledgement.
    I referenced the URL for the product used from IC Station and made the
    setting of the pins explicit. NOTE - it can go to a DIGITAL pin on
    receive. (although it is an analog signal). Anyhoo - it worked - hello
    world transmitted from one to the other.

--- arduino-prototype/RF_VW_RX/RF_VW_RX.ino
+++ arduino-prototype/RF_VW_RX/RF_VW_RX.ino
@@ -1,20 +1,15 @@
-// Simple example of how to use VirtualWire to receive messages
-// Implements a simplex (one-way) receiver with an Rx-B1 module
-//
-// See VirtualWire.h for detailed API docs
-// Author: Mike McCauley
-// Copyright (C) 2008 Mike McCauley
-// $Id: receiver.pde,v 1.3 2009/03/30 00:07:24 mikem Exp $
+// based on Mike McAuley's VirtualWire example code
+// uses the ICStation 433 MHz modules: http://www.icstation.com/433mhz-transmitter-receiver-arduino-project-p-1402.html
+// Wire 5V to the VCC Pin on the receiver.
+// Wire pin 11 (or whatever you configure below) to the data pin.
+// I didn't need antennas to get this and the tx sketch working, but if you put any distance, then 10cm coiled antenna.
+// 
 
 #include "VirtualWire.h" 
-#undef int
-#undef abs
-#undef double
-#undef float
-#undef round
 
 long countIdle = 0;
 int charCount = 0;
+const int RX_PIN = 11;
 
 void setup()
 {
@@ -22,6 +17,7 @@ void setup()
     Serial.println("setup");
 
     // Initialise the IO and ISR
+    vw_set_rx_pin(RX_PIN); // the device is connected to pin 11.
     vw_set_ptt_inverted(true); // Required for DR3100
     vw_setup(2000);  // Bits per sec
 
@@ -29,44 +25,57 @@ void setup()
     Serial.println("finished setup");
 }
 
+
 void loop()
 {
-    uint8_t buf[VW_MAX_MESSAGE_LEN];
-    uint8_t buflen = VW_MAX_MESSAGE_LEN;
-
-   if (!vw_have_message()) {
-     countIdle += 1;
-     
-     if (countIdle > 60000 ) // every few rounds, go another line.
-     {
-        countIdle = 0;
-        Serial.print(".");
-        charCount +=1; 
-        if (charCount > 40)
-        {
-          Serial.println(".");
-          charCount = 0;
-        }
-     }
+   if (vw_have_message() != true) {
+     showActivity();
    } else {
-     Serial.println("must be a message");
-     countIdle = 0;
-     
-     if (vw_get_message(buf, &buflen)) // Non-blocking
-     {
-        int i;
-  
-        digitalWrite(13, true); // Flash a light to show received good message
-        // Message with a good checksum received, dump it.
-        Serial.print("Got ("); Serial.print(buflen); Serial.print("):");
-    
-        for (i = 0; i < buflen; i++)
-        {
-            Serial.print((char)buf[i]);
-            Serial.print(" ");
-        }
-        Serial.println("");
-        digitalWrite(13, false);
-     }
+     dumpMsg();
    }
 }
+
+void dumpMsg()
+{
+  uint8_t buf[VW_MAX_MESSAGE_LEN];
+  uint8_t buflen = VW_MAX_MESSAGE_LEN;
+  resetIdleActivity();
+  
+  if (vw_get_message(buf, &buflen)) // Non-blocking
+  {
+    int i;
+    Serial.println();
+    digitalWrite(13, true); // Flash a light to show received good message
+    // Message with a good checksum received, dump it.
+    Serial.print("Got ("); Serial.print(buflen); Serial.print(") bytes:");
+  
+    for (i = 0; i < buflen; i++)
+    {
+        Serial.print((char)buf[i]);
+    }
+    Serial.println();
+    digitalWrite(13, false);
+  }
+}
+
+void resetIdleActivity()
+{
+  countIdle = 0;
+}
+
+void showActivity() {
+  countIdle += 1;
+     
+  if (countIdle > 60000 ) // every few rounds, go another line.
+  {
+    resetIdleActivity();
+    Serial.print(".");
+    charCount +=1; 
+    if (charCount > 40)
+    {
+      Serial.println(".");
+      charCount = 0;
+    }
+  }
+}
+
--- arduino-prototype/RF_VW_TX/RF_VW_TX.ino
+++ arduino-prototype/RF_VW_TX/RF_VW_TX.ino
@@ -1,25 +1,19 @@
-// transmitter.pde
-//
-// Simple example of how to use VirtualWire to transmit messages
-// Implements a simplex (one-way) transmitter with an TX-C1 module
-//
-// See VirtualWire.h for detailed API docs
-// Author: Mike McCauley
-// Copyright (C) 2008 Mike McCauley
-// $Id: transmitter.pde,v 1.3 2009/03/30 00:07:24 mikem Exp $
-
+// transmitter based on the original virtualwire transmitter example from Mike McCauley
+// uses 433 MHz modules from IC Station: http://www.icstation.com/433mhz-transmitter-receiver-arduino-project-p-1402.html
+// power with 5V. 
+// connect data pin to pin 12 (or whatever you set)
+// I didn't need an antenna to get this going, but that's with devices 20cm from eachother.
+// 
 #include "VirtualWire.h"
-#undef int
-#undef abs
-#undef double
-#undef float
-#undef round
+
+const int TX_PIN = 12;
 void setup()
 {
     Serial.begin(9600);    // Debugging only
     Serial.println("setup");
 
     // Initialise the IO and ISR
+    vw_set_tx_pin(TX_PIN);
     vw_set_ptt_inverted(true); // Required for DR3100
     vw_setup(2000);  // Bits per sec
     Serial.println("setup complete");
@@ -27,7 +21,7 @@ void setup()
 
 void loop()
 {
-    const char *msg = "LMR";
+    const char *msg = "Hello World";
     digitalWrite(13, true); // Flash a light to show transmitting
     Serial.print("Transmitting: "); Serial.println(msg);
     delay(150);
